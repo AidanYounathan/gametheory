f042cc31db8e4bfd593e0eaaecf352f8e2810d06
Aidan <ayounathan05@gmail.com>
2025-09-26 02:54:27 -0700

    Initial commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,19 @@
+export default function Header() {
+    return (
+        <header className="w-full ">
+            <div className="rounded-[5px] m-5 border-5 border-[var(--bone)] text-[var(--bone)]">   
+            <div className="flex justify-between">
+                <div className="flex items-center p-3">
+                    <h1 className="font-bold text-4xl">
+                        Game Theory
+                    </h1>
+                </div>
+                <div className="text-[32px] flex gap-10 items-center p-3 font-medium">
+                    <a href="">About</a>
+                    <a href="">Play Now</a>
+                </div>
+            </div>
+            </div>
+        </header>
+    )
+}
