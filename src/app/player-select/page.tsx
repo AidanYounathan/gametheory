00127a9371fa882c44a0b604df4eaaa0cdf2a0da
Aidan <ayounathan05@gmail.com>
2025-10-18 20:29:29 -0700

    hu

--- src/app/player-select/page.tsx
+++ src/app/player-select/page.tsx
@@ -1,5 +1,15 @@
-export default function Home() {
+import Marquee from "react-fast-marquee";
 
+export default function Home() {
+    return (
+        <div className=" text-bone">
+            <Marquee className="overflow-hidden font-bold text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl">Welcome to the Character Select Screen!</Marquee>
+            <p className="mt-4">Choose your character:</p>
+            <div className="grid grid-cols-2 gap-4 mt-8 ">
+                {/* Character options will go here */}
+            </div>
+        </div>
+    );
 }
 
 //Need to design character select, then work on the logic, then building it out
