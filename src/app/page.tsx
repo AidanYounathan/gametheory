f047f58236150d721f2c9b7351e060c39fbe1512
Aidan <ayounathan05@gmail.com>
2025-10-27 20:30:07 -0700

    YESSSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import Link from "next/link";
 import Header from "./components/header";
 import Marquee from "react-fast-marquee";
-import {Slider} from "@heroui/react";
 
 
 
